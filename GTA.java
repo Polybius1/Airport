911617ad00121ce315d63dded06813c6a69d0f78
Polybius1 <dumpypotatoes@gmail.com>
2015-03-04 14:38:16 -0800

    Update GTA.java

--- GTA.java
+++ GTA.java
@@ -2,7 +2,6 @@ class GTAdriver{
   
   public static void main(String[] args) 
   {
-       
     Airport.readAirports();
     Flight.readFlights();
     //PrintSystem.printSys(); //comment this out for Airmap test
