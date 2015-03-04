dbc51a18d8c75a4b181e8ee717a88dad01eccb1e
Polybius1 <dumpypotatoes@gmail.com>
2015-03-04 12:18:56 -0800

    Create GTA.java

---
+++ GTA.java
@@ -0,0 +1,15 @@
+class GTAdriver{
+  
+  public static void main(String[] args) 
+  {
+       
+    Airport.readAirports();
+    Flight.readFlights();
+    //PrintSystem.printSys(); //comment this out for Airmap test
+    Airport.nudgeAirports(); //nudge the airports to adjust their lat and lon for map readability 
+    AirMap.showMap();
+        
+  }
+}
+
+
